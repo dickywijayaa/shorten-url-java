62a8c388f5bd553f407f42b5a14430ad7a7c5fac
dickywijayaa <dicky.wijaya@ralali.com>
2020-05-22 22:22:29 +0700

    first init

---
+++ src/main/java/com/example/shortenurl/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.shortenurl;
+
+public class Greeting {
+
+    private final String name;
+ 
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
