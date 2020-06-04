4deaaead667f2e2dce785707a8b69783c9803096
dickywijayaa <dicky.wijaya@ralali.com>
2020-06-04 09:46:21 +0700

    move message to constant + function to return redirect code

---
+++ src/main/java/com/example/constants/Message.java
@@ -0,0 +1,15 @@
+package com.example.constants;
+
+public class Message {
+    
+    public String INTERNAL_SERVER_ERROR_MESSAGE = "something went wrong";
+    public String REQUIRED_URL_INPUT = "url is required";
+    public String REQUIRED_SHORTCODE_INPUT = "shortcode is required";
+    public String INVALID_REGEX_SHORTCODE_INPUT = "invalid shortcode pattern";
+    public String SHORTCODE_ALREADY_EXISTS_IN_DATABASE = "code already exists in database.";
+    public String SHORTCODE_NOT_EXISTS_IN_DATABASE = "code not found in database.";
+    public String INVALID_URL_PREFIX = "url prefix must be started with http(s):// !";
+
+    public String SUCCESS_INSERT_DATA = "successfully insert data";
+
+}
--- src/main/java/com/example/shortenurl/ShortenController.java
+++ src/main/java/com/example/shortenurl/ShortenController.java
@@ -1,9 +1,14 @@
 package com.example.shortenurl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import com.example.constants.Message;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,41 +25,55 @@ public class ShortenController {
     ShortenRepository shortenRepository;
 
     @RequestMapping(value = "/{code}", method = RequestMethod.GET)
-    public Shorten GetURLFromShortcode(@PathVariable String code) {
-        return shortenRepository.findByShortcode(code);
+    public ResponseEntity<Object> GetURLFromShortcode(@PathVariable String code) throws URISyntaxException {
+        Shorten data = shortenRepository.findByShortcode(code);
+
+        if (data == null) {
+            Message message = new Message();
+            Response result = new Response(message.SHORTCODE_NOT_EXISTS_IN_DATABASE);
+
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+
+        URI url = new URI(data.getUrl());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(url);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public ResponseEntity<Response> PostShorten(@RequestBody Map<String, String> payload) {
+        Message message = new Message();
+
         String url = payload.get("url");
         String shortcode = payload.get("shortcode");
 
-        Response result = new Response("something went wrong");
+        Response result = new Response(message.INTERNAL_SERVER_ERROR_MESSAGE);
 
         if (url == null) {
-            result.message = "url is required";
+            result.message = message.REQUIRED_URL_INPUT;
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
-        
+
         if (shortcode == null) {
-            result.message = "shortcode is required";
+            result.message = message.REQUIRED_SHORTCODE_INPUT;
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
 
         if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            result.message = "url prefix must be started with http(s):// !";
+            result.message = message.INVALID_URL_PREFIX;
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
 
         if (!Pattern.matches("^[0-9a-zA-Z_]{6}$", shortcode)) {
-            result.message = "invalid shortcode pattern";
+            result.message = message.INVALID_REGEX_SHORTCODE_INPUT;
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
 
         Shorten data = new Shorten(url, shortcode);
         shortenRepository.save(data);
 
-        result.message = "successfully insert data";
+        result.message = message.SUCCESS_INSERT_DATA;
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
